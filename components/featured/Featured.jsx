5157647ecbf490176c122ac5c4f955f5e111cc23
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2023-11-09 23:26:09 +0530

    Add files via upload

---
+++ components/featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Sameer Khan here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+      <div className={styles.videoContainer}>
+      <video className={styles.video} controls>
+            <source src="/nature.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet alim consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Cupiditate, quam nisi magni ea laborum inventore voluptatum
+            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
+            quisquam! Harum unde sit culpa debitis.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
