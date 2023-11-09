e680e25511f372f3b3420a72d7135df24b1fbdb6
sameerpw43 <89029265+sameerpw43@users.noreply.github.com>
2023-11-09 23:24:49 +0530

    Add files via upload

---
+++ utils/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlIPLhjFy6REwvbyCrDMFhKLWnDVEVAKw",
+  authDomain: "blog-2d3de.firebaseapp.com",
+  projectId: "blog-2d3de",
+  storageBucket: "blog-2d3de.appspot.com",
+  messagingSenderId: "310399751033",
+  appId: "1:310399751033:web:e4e998ae44d2fddcc815a8",
+  measurementId: "G-GWT2L07B32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
